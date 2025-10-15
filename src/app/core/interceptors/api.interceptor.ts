4fdcb526f8d20f1b7a0e661be3424e0a1724c516
Ishak ates <ishakfeuer@gmail.com>
2025-10-15 10:30:34 +0200

    Remove src/environments from tracking and update .gitignore

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -6,6 +6,7 @@ import {
   HttpRequest,
 } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({ providedIn: "root" })
 export class ApiInterceptor implements HttpInterceptor {
@@ -13,10 +14,9 @@ export class ApiInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const apiReq = req.clone({ url: `http://localhost:8000/api${req.url}` });
+    const apiReq = req.clone({ url: `${environment.API_URL}${req.url}` });
+    console.log(apiReq);
+    
     return next.handle(apiReq);
   }
 }
-
-// { url: `https://api.realworld.io/api${req.url}` }
-// { url: `http://localhost:8000/api${req.url}` }
